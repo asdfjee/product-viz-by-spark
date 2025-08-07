b35ff94ac01a70983d96e178cfe31cc499314a74
asdfjee <inajee88@gmail.com>
2025-08-07 12:07:08 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from "react-error-boundary";
 
-
 import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
 
